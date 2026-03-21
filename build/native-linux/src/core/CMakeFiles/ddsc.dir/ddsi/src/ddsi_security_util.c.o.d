c27453f9af8fe9d2e3100c7c722496bf3f9f155d
Matheus Zeitune <matheus.zeitune.developer@gmail.com>
2026-03-20 23:32:05 -0300

    chore: remove native-linux build artifacts from tracking

--- build/native-linux/src/core/CMakeFiles/ddsc.dir/ddsi/src/ddsi_security_util.c.o.d
+++
@@ -1,4 +0,0 @@
-src/core/CMakeFiles/ddsc.dir/ddsi/src/ddsi_security_util.c.o: \
- /mnt/e/TI/git/tese/cyclonedds/src/core/ddsi/src/ddsi_security_util.c \
- /usr/include/stdc-predef.h \
- /mnt/e/TI/git/tese/CycloneDds.NET/build/native-linux/src/ddsrt/include/dds/features.h
